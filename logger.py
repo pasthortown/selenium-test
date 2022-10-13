e8ee466328aac844924efed98ca36f81423fa38a
pasthortown <pastorchessmaster@gmail.com>
2022-10-13 12:42:44 -0500

    limpio

--- logger.py
+++ logger.py
@@ -4,7 +4,7 @@ import shutil
 class Logger:
     def __init__(self, output_folder, filename):
         self.output_folder = output_folder
-        shutil.rmtree(output_folder)
+        shutil.rmtree(output_folder,True)
         os.mkdir(output_folder)
         os.mkdir(output_folder + '/documentos')
         os.mkdir(output_folder + '/proceso')
